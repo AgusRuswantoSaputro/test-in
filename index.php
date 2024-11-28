4bda10cd769e4f9af77f8e40498fdd046b699e8b
Saputro <agus78rs@gmail.com>
2024-11-28 14:49:18 +0700

    menambahkan file index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test in ke github</title>
+</head>
+<body>
+    <h1>Mencoba untuk push ke github</h1>
+</body>
+</html>
