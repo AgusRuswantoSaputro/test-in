75d1aa29725d05f0d0415b3d904741d105270424
Saputro <agus78rs@gmail.com>
2024-11-28 15:00:51 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 </head>
 <body>
     <h1>Mencoba untuk push ke github</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad repudiandae dolores facilis non. Corrupti velit praesentium quasi deleniti quis soluta recusandae officia, optio fuga. Voluptatem, dignissimos excepturi. Quam, adipisci vel.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
